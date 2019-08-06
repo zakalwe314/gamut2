1dc4e4ecf6137d322f9d3bbe26161ab2d3794e0f
Euan <euan.smith@lightblueoptics.com>
2019-08-06 16:55:51 +0100

    initial components mostly there - just need to add loading data

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,190 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <v-app>
+    <input type="file" style="display:none" ref="file">
+    <v-app-bar app dense>
+      <v-toolbar-items>
+
+        <v-menu bottom right offset-y>
+          <template v-slot:activator="{on}">
+            <v-btn text v-on="on">Test Data</v-btn>
+          </template>
+          <v-list>
+            <v-list-item @click="">Import...</v-list-item>
+            <v-divider></v-divider>
+            <v-list-item @click="" v-for="set of sets" :key="set">{{set}}<v-icon v-if="testData.name===set">mdi-check</v-icon></v-list-item>
+          </v-list>
+        </v-menu>
+
+        <v-menu bottom right offset-y>
+          <template v-slot:activator="{on}">
+            <v-btn text v-on="on">Reference Data</v-btn>
+          </template>
+          <v-list>
+            <v-list-item  @click="">Import...</v-list-item>
+            <v-divider></v-divider>
+            <v-list-item @click="" v-for="set of sets" :key="set">{{set}}<v-icon v-if="refData.name===set">mdi-check</v-icon></v-list-item>
+          </v-list>
+        </v-menu>
+
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Gamut 2</span>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <canvas ref="canvas"></canvas>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+  canvas{
+    width:100%;
+    height:100%;
+  }
 </style>
+
+<script>
+  let scene, canvas, camera, controls, renderer;
+
+
+  import {mapGetters} from 'vuex';
+  export default {
+    name: 'App',
+    data: () => ({
+    }),
+    computed:{
+      ...mapGetters(['sets','refData','testData'])
+    },
+    methods:{
+      animate(){
+        requestAnimationFrame( this.animate );
+        controls.update();
+        renderer.render( scene, camera );
+      },
+      updateScene(){
+        const mesh = makeCIELabMesh(this.testData);
+        const wire = makeWireFrame(makeCIELabMesh(this.refData));
+        scene = new THREE.Scene();
+        scene.add(mesh);
+        scene.add(wire);
+      }
+    },
+    mounted(){
+      canvas = this.$refs.canvas;
+      camera = makeCamera();
+      controls = new THREE.OrbitControls(camera, canvas);
+      renderer = new THREE.WebGLRenderer({canvas});
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      this.updateScene();
+      this.animate();
+
+      window.addEventListener( 'resize', onWindowResize, false );
+
+      function onWindowResize(){
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+      }
+    }
+  };
+
+
+  function makeWireFrame(mesh){
+    const helper                = new THREE.WireframeHelper(mesh);
+    helper.material.depthTest   = false;
+    helper.material.opacity     = 0.25;
+    helper.material.transparent = true;
+    return helper;
+  }
+  function makeCamera(){
+    let camera,
+      width = window.innerWidth,
+      height = window.innerHeight,
+      fov, ratio, near, far;
+
+    // origin camera
+    fov = 22.5;
+    ratio = width / height;
+    near = 1;
+    far = 1000;
+    camera = new THREE.OrthographicCamera(
+      -width / 4, width / 4, height / 4, height / -4, near, far
+    );
+    camera.position.set(200, 300, 200);
+    camera.lookAt(new THREE.Vector3(0, 50, 0));
+    return camera;
+  }
+  function makeCIELabMesh(s){
+    const {RGB, XYZ, TRI} = s;
+    const cols = RGB.map(c => (c[0]<<16) + (c[1]<<8) + c[2]);
+    const offset = sumArrays(RGB).map(v=>-v/RGB.length);
+    const points = offsetArrays(RGB,offset).map(p=>unitVector(p));
+
+    const max = maxArray(XYZ, a=>a[1]);
+    const bla = normArrays(XYZ,max).map(xyz2lab).map(p=>[p[2],p[0],p[1],]);
+
+    const geometry = new THREE.Geometry();
+    for (let i = 0; i < points.length; i += 1) {
+      geometry.vertices.push(new THREE.Vector3().fromArray(bla[i]));
+      //geometry.vertexColors.push(new THREE.Color(cols[i]));
+    }
+    let normal;
+    for (let i = 0; i < TRI.length; i += 1) {
+      const a = new THREE.Vector3().fromArray(bla[TRI[i][0]]);
+      const b = new THREE.Vector3().fromArray(bla[TRI[i][1]]);
+      const c = new THREE.Vector3().fromArray(bla[TRI[i][2]]);
+      normal  = new THREE.Vector3()
+        .crossVectors(
+          new THREE.Vector3().subVectors(b, a),
+          new THREE.Vector3().subVectors(c, a)
+        )
+        .normalize();
+      geometry.faces.push(
+        new THREE.Face3(TRI[i][0], TRI[i][1], TRI[i][2], normal, [new THREE.Color(cols[TRI[i][0]]),new THREE.Color(cols[TRI[i][1]]),new THREE.Color(cols[TRI[i][2]])])
+      );
+    }
+
+    return new THREE.Mesh(
+      geometry,
+      new THREE.MeshBasicMaterial( { vertexColors:THREE.VertexColors })
+    );
+  }
+  function labF(v) {
+    return v <= 0.008856 ? v * 7.787 + 16 / 116 : Math.pow(v, 1 / 3);
+  }
+  function xyz2lab(a){
+    const fy=labF(a[1]);
+    return [116*fy-16, 500*(labF(a[0])-fy), 200*(fy-labF(a[2]))];
+  }
+  function sumArrays(d){
+    return d.reduce((a,b)=>a?a.map((v,i)=>v+b[i]):b);
+  }
+
+  function offsetArrays(d,o){
+    return d.map(a=>a.map((v,i)=>v+o[i]));
+  }
+
+  function mag(a){
+    return Math.sqrt(a.reduce((t,v)=>t+v*v,0));
+  }
+
+  function unitVector(a){
+    const d=mag(a);
+    return a.map(v=>v/d);
+  }
+
+  function maxArray(a,fn){
+    return a.reduce((m,dat)=>{
+      const r={val:fn(dat),dat};
+      return m && m.val>=r.val ? m : r;
+    },null).dat;
+  }
+  function normArrays(d,n){
+    return d.map(a=>a.map((v,i)=>v/n[i]));
+  }
+
+</script>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,95 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const refs = {
+  rec2020: require('./rec2020.json')
+};
+
+const state = {
+  dataSets:Object.keys(refs).map(k=>refData2dataSet(refs[k],k)),
+  refData:0,
+  testData:0,
+};
+
+const mutations = {
+
+};
+
+const actions = {
+
+};
+
+const getters = {
+  refData:state=>state.dataSets[state.refData],
+  testData:state=>state.dataSets[state.testData],
+  sets:state=>state.dataSets.map(ds=>ds.name),
+};
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  state,
+  mutations,
+  actions,
+  getters,
 })
+
+function refData2dataSet(refData,name){
+  function mixrgb(rgb1,rgb2,lin){
+    return [
+      rgb1[0]*(1-lin)+rgb2[0]*lin,
+      rgb1[1]*(1-lin)+rgb2[1]*lin,
+      rgb1[2]*(1-lin)+rgb2[2]*lin,
+    ]
+  }
+  const gs=refData.GS.sort((a,b)=>a-b),mx=gs[gs.length-1],mn=gs[0];
+  const gs2lin=v=>Math.pow((v-mn)/(mx-mn),refData.gamma);
+  const {RGB,TRI} = makeTesselation(gs);
+  //First get the primaries in order 0K 1B 2G 3C 4R 5M 6Y 7W
+  const primaries = refData.primaries.sort((a,b)=>a[0]!==b[0]?a[0]-b[0]: a[1]!==b[1]?a[1]-b[1] : a[2]-b[2]);
+  //Now for each RGB value get the XYZ from the supplied primaries
+  const XYZ = RGB.map(function(rgb){
+    const lr=gs2lin(rgb[0]),lg=gs2lin(rgb[1]),lb=gs2lin(rgb[2]);
+    const pr=[];
+    for(let n=0;n<4;n++) pr.push(mixrgb(primaries[n].slice(3),primaries[n+4].slice(3),lr));
+    for(let n=0;n<2;n++) pr.push(mixrgb(pr[n],pr[n+2],lg));
+    return mixrgb(pr[4],pr[5],lb);
+  });
+  return {RGB, XYZ, TRI, name};
+}
+
+function makeTesselation(gs){
+  const n=gs.length-1;
+  const mx=gs[n], mn=gs[0];
+  const faces = [
+    [mn,0,1,2],
+    [mn,2,0,1],
+    [mn,1,2,0],
+    [mx,0,2,1],
+    [mx,1,0,2],
+    [mx,2,1,0],
+  ];
+  const TRI=[],RGB=[];
+  for (let face of faces){
+    const st=RGB.length;
+    for(let q=0;q<=n;q++){
+      for(let p=0;p<=n;p++){
+        const rgb=[0,0,0];
+        rgb[face[1]]=face[0];
+        rgb[face[2]]=gs[q];
+        rgb[face[3]]=gs[p];
+        RGB.push(rgb);
+      }
+    }
+    for(let q=0;q<n;q++){
+      for(let p=0;p<n;p++){
+        const m=st + (n+1)*q + p;
+        TRI.push(
+          [m,m+n+1,m+1],
+          [m+n+1,m+n+2,m+1],
+        )
+      }
+    }
+  }
+  return {TRI,RGB};
+}
